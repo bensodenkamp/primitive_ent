8631424352776184f5b159199b788bdf2291f3f7
Benjamin Sodenkamp <54522068+bensodenkamp@users.noreply.github.com>
2023-04-29 11:50:24 -0600

    validation updates

--- primitive/color.go
+++ primitive/color.go
@@ -27,22 +27,35 @@ func MakeHexColor(x string) Color {
 	b = 0
 	a = 0
 	a = 255
+	err := new(error)
 	switch len(x) {
 	case 3:
-		fmt.Sscanf(x, "%1x%1x%1x", &r, &g, &b)
+		_, *err = fmt.Sscanf(x, "%1x%1x%1x", &r, &g, &b)
+		if err != nil {
+			return Color{r, g, b, a}
+		}
 		r = (r << 4) | r
 		g = (g << 4) | g
 		b = (b << 4) | b
 	case 4:
-		fmt.Sscanf(x, "%1x%1x%1x%1x", &r, &g, &b, &a)
+		_, *err = fmt.Sscanf(x, "%1x%1x%1x%1x", &r, &g, &b, &a)
+		if err != nil {
+			return Color{r, g, b, a}
+		}
 		r = (r << 4) | r
 		g = (g << 4) | g
 		b = (b << 4) | b
 		a = (a << 4) | a
 	case 6:
-		fmt.Sscanf(x, "%02x%02x%02x", &r, &g, &b)
+		_, *err = fmt.Sscanf(x, "%02x%02x%02x", &r, &g, &b)
+		if err != nil {
+			return Color{r, g, b, a}
+		}
 	case 8:
-		fmt.Sscanf(x, "%02x%02x%02x%02x", &r, &g, &b, &a)
+		_, *err = fmt.Sscanf(x, "%02x%02x%02x%02x", &r, &g, &b, &a)
+		if err != nil {
+			return Color{r, g, b, a}
+		}
 	}
 	return Color{r, g, b, a}
 }
